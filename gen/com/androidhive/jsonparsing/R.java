218bd662e9a23d7c02c3e2da6eb29b88800c88c5
Laumond Arnaud <arnaud.laumond@gmail.com>
2013-12-04 21:06:18 +0100

    changements

--- gen/com/androidhive/jsonparsing/R.java
+++ gen/com/androidhive/jsonparsing/R.java
@@ -14,12 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int email=0x7f050001;
-        public static final int email_label=0x7f050004;
-        public static final int mobile=0x7f050002;
-        public static final int mobile_label=0x7f050005;
-        public static final int name=0x7f050000;
-        public static final int name_label=0x7f050003;
+        public static final int android_picture=0x7f050000;
+        public static final int email=0x7f050002;
+        public static final int email_label=0x7f050005;
+        public static final int mobile=0x7f050003;
+        public static final int mobile_label=0x7f050006;
+        public static final int name=0x7f050001;
+        public static final int name_label=0x7f050004;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
